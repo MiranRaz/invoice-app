0614daf0f19f72a1bcd383b4a3e99d188ccd42d9
Miran Raznatovic <miran@bloola.com>
2024-10-09 19:29:26 +0200

    styling changes, added status filter implementation, added total number of invoices calculation function...

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatOption, MatSelect } from '@angular/material/select';
@@ -22,7 +22,26 @@ import { DUMMY_INVOICES } from '../../dummy-data/dummy-invoices';
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  statuses = new FormControl('');
-  statusesList: string[] = ['Draft', 'Pending', 'Paid'];
   invoices = DUMMY_INVOICES;
+  statuses = new FormControl<string[]>([]);
+  statusesList: string[] = ['Draft', 'Pending', 'Paid'];
+
+  totalInvoices = computed(() => this.filteredInvoices().length);
+
+  selectedStatusesSignal = signal<string[]>([]);
+  filteredInvoices = computed(() => {
+    const selectedStatuses = this.selectedStatusesSignal();
+    if (!selectedStatuses || selectedStatuses.length === 0) {
+      return this.invoices;
+    }
+    return this.invoices.filter((invoice) =>
+      selectedStatuses.includes(invoice.status)
+    );
+  });
+
+  ngOnInit() {
+    this.statuses.valueChanges.subscribe((selectedStatuses) => {
+      this.selectedStatusesSignal.set(selectedStatuses || []);
+    });
+  }
 }
